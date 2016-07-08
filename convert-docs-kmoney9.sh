cd1144207af04231397a2506b4867a80478e465d
kevinjsails <kevinjsails@gmail.com>
2016-07-08 13:39:37 -0400

    message convert

--- convert-docs-kmoney9.sh
+++ convert-docs-kmoney9.sh
@@ -1,13 +1,13 @@
-
-
+#!/bin/bash
 # Declare input argument as a variable
-
+FILE=$1
 # Convert markdown to HTML
-
+pandoc -o $FILE.html $FILE.md
 # Convert markdown to DOCX
-
+pandoc -o $FILE.docx $FILE.md
 # Convert markdown to ODT
-
+pandoc -o $FILE.odt $FILE.md
 # Convert markdown to PDF
-
+pandoc -o $FILE.pdf $FILE.md
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
+echo "Converted $FILE.md to HTML, DOCX, ODT, PDF"
